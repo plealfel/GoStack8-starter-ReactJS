4f30135ff0ae204ce8b847abbc8c1c95bc828789
pefelizardo <pefelizardo@prestadores.uhgbrasil.com.br>
2020-03-25 13:54:43 -0300

    Adição de exercício de reactjs

---
+++ huntweb/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Routes from "./routes"
+
+import Main from "./pages/main";
+
+import "./styles.css"
+
+import Header from "./components/Header";
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
+
+export default App;
